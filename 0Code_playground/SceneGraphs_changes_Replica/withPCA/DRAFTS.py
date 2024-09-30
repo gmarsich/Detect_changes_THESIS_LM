e6849b189fd0cbb9c83355a1a021fd9aab6ddd56
Gaia Marsich <g.marsich@gmail.com>
2024-09-30 10:47:53 +0200

    Fix farthest_point_sampling for colors; add downsampling

--- 0Code_playground/SceneGraphs_changes_Replica/withPCA/DRAFTS.py
+++ 0Code_playground/SceneGraphs_changes_Replica/withPCA/DRAFTS.py
@@ -1,7 +1,11 @@
 import open3d as o3d
+import numpy as np
 
-colored_point_cloud = o3d.io.read_point_cloud('/local/home/gmarsich/Desktop/Thesis/0Code_playground/SceneGraphs_changes_Replica/withPCA/preprocessing/results/frl_apartment_0_withIDs.ply')
-colored_point_cloud_2 = o3d.io.read_point_cloud('/local/home/gmarsich/Desktop/Thesis/0Code_playground/SceneGraphs_changes_Replica/withPCA/preprocessing/results/frl_apartment_1_withIDs.ply')
-o3d.visualization.draw_geometries([colored_point_cloud, colored_point_cloud_2])
+# colored_point_cloud = o3d.io.read_point_cloud('/local/home/gmarsich/Desktop/Thesis/0Code_playground/SceneGraphs_changes_Replica/withPCA/preprocessing/results/frl_apartment_0_withIDs.ply')
+# colored_point_cloud_2 = o3d.io.read_point_cloud('/local/home/gmarsich/Desktop/Thesis/0Code_playground/SceneGraphs_changes_Replica/withPCA/preprocessing/results/frl_apartment_1_withIDs.ply')
+# o3d.visualization.draw_geometries([colored_point_cloud, colored_point_cloud_2])
 
 
+distances = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+distances = np.delete(distances, 1, axis=1)
+print(distances)
--- 0Code_playground/SceneGraphs_changes_Replica/withPCA/PCA.py
+++ 0Code_playground/SceneGraphs_changes_Replica/withPCA/PCA.py
@@ -21,6 +21,11 @@ path_listInstances_1 = '/local/home/gmarsich/Desktop/data_Replica/frl_apartment_
 objectIDs_0 = [34, 39, 27, 103, 38, 164] # 1: # bike, bike, ceiling, sofa, cup, sink
 objectIDs_1 = [77, 93, 10, 4, 66, 59] # 0: bike, bike, ceiling, sofa, mat, book
 
+objectID_0 = 4
+objectID_1 = 121
+
+number_components = 2
+
 
 
 #
@@ -44,6 +49,7 @@ def pca_embedding(pcd, n_components=3):
 def farthest_point_sampling(pcd, n_samples):
 
     points = np.asarray(pcd.points)
+    colors = np.asarray(pcd.colors)
     n_points = points.shape[0]
     
     sampled_indices = np.zeros(n_samples, dtype=int)
@@ -51,44 +57,90 @@ def farthest_point_sampling(pcd, n_samples):
 
     distances = np.full(n_points, np.inf)
 
-    for i in range(0, n_samples):
-        # Update distances for the current selected point
-        current_point = points[sampled_indices[i]]
+    for i in range(1, n_samples):
+        current_point = points[sampled_indices[i - 1]]
         distances = np.minimum(distances, np.linalg.norm(points - current_point, axis=1))
-        
-        # Select the farthest point
-        sampled_indices[i] = np.argmax(distances)
+        sampled_indices[i] = np.argmax(distances) # select the farthest point
 
     # Create a new point cloud from the sampled points
     sampled_points = points[sampled_indices]
+    sampled_colors = colors[sampled_indices]
     sampled_pcd = o3d.geometry.PointCloud()
     sampled_pcd.points = o3d.utility.Vector3dVector(sampled_points)
+    sampled_pcd.colors = o3d.utility.Vector3dVector(sampled_colors)
 
     return sampled_pcd
 
+
+def downsample_pair_pcd(pcd_0, pcd_1):
+    num_points_0 = len(pcd_0.points)
+    num_points_1 = len(pcd_1.points)
+
+    if num_points_0 > num_points_1:
+        pcd_0_downsampled = farthest_point_sampling(pcd_0, num_points_1)
+        pcd_1_downsampled = pcd_1
+    else:
+        pcd_1_downsampled = farthest_point_sampling(pcd_1, num_points_0)
+        pcd_0_downsampled = pcd_0
+
+    return pcd_0_downsampled, pcd_1_downsampled
 
 
 #
 # Performing PCA and comparing the results
 #
 
+# Load the scene graph and the point clouds. Apply the tranformation to try to align the two point clouds
+
 scene_graph_0 = sceneGraph(path_plyFile_0, path_listInstances_0)
 scene_graph_1 = sceneGraph(path_plyFile_1, path_listInstances_1)
 
-pcd_0 = scene_graph_0.get_pointCloud(4)
-pcd_1 = scene_graph_1.get_pointCloud(121)
+pcd_0 = scene_graph_0.get_pointCloud(objectID_0)
+pcd_1 = scene_graph_1.get_pointCloud(objectID_1)
 
 transformationMatrix = get_transformationMatrix(pcd_0, pcd_1, seeRenderings = False) # a downsampling is performed internally to compute the matrix
 pcd_1.transform(transformationMatrix)
 o3d.visualization.draw_geometries([pcd_0, pcd_1])
 
-emb_0 = pca_embedding(pcd_0)
-emb_1 = pca_embedding(pcd_1)
+
+# Compute the embeddings; cosine similarity as a metric
+
+pcd_0_downsampled, pcd_1_downsampled = downsample_pair_pcd(pcd_0, pcd_1) # so that the embeddings can be better compared. The new pcds have the same amount of points
+
+pcd_0_downsampled = farthest_point_sampling(pcd_0_downsampled, n_samples=1000)
+pcd_1_downsampled = farthest_point_sampling(pcd_1_downsampled, n_samples=1000)
+
+emb_0 = pca_embedding(pcd_0_downsampled, n_components=number_components)
+emb_1 = pca_embedding(pcd_1_downsampled, n_components=number_components)
+
+# pcd = o3d.geometry.PointCloud()
+# pcd.points = o3d.utility.Vector3dVector(emb_0)
+# pcd_1 = o3d.geometry.PointCloud()
+# pcd_1.points = o3d.utility.Vector3dVector(emb_1)
+# o3d.visualization.draw_geometries([pcd.paint_uniform_color([1, 0.706, 0]), pcd_1.paint_uniform_color([0, 0.651, 0.929])])
+
+cos_sim = cosine_similarity(emb_0, emb_1)
+cos_sim_max = []
+while cos_sim.size > 0:
+    max_value = np.max(cos_sim)
+    max_index = np.unravel_index(np.argmax(cos_sim), cos_sim.shape) # get row and column of the max
+
+    cos_sim_max.append(max_value)
+
+    cos_sim = np.delete(cos_sim, max_index[0], axis=0)  # remove the row
+    cos_sim = np.delete(cos_sim, max_index[1], axis=1)  # remove the column
+
+average_similarity = np.mean(cos_sim_max)
+print("Average Cosine Similarity:", average_similarity)
 
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(emb_1)
-o3d.visualization.draw_geometries([pcd])
+
+
+
+
+# cos_sim_min = np.max(cos_sim, axis=1) # get the maximum value across each row
+# average_similarity = np.mean(cos_sim_min)
+# print("Average Cosine Similarity:", average_similarity)
 
 
 
@@ -97,17 +149,27 @@ o3d.visualization.draw_geometries([pcd])
 
 
 
-mean_emb_0 = np.mean(emb_0, axis=0)
-mean_emb_1 = np.mean(emb_1, axis=0)
-distance = np.linalg.norm(mean_emb_0 - mean_emb_1)
-print(distance)
+
+# mean_emb_0 = np.mean(emb_0, axis=0)
+# mean_emb_1 = np.mean(emb_1, axis=0)
+# distance = np.linalg.norm(mean_emb_0 - mean_emb_1)
+# print(distance)
+
+
+
 
 
 # Euclidean distance
 
-# Cosine similarity
+
 
 # Chamfer distance
+
+# Hausdorff Distance
+
+# Wasserstein Distance
+
+# Kolmogorov-Smirnov (K-S) test
 
 
 # TODO: downsampling
