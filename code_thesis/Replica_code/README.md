7661f8731269318dda28d8a14eb9fda3f80fe6d9
Gaia Marsich <g.marsich@gmail.com>
2024-10-06 21:45:28 +0200

    Got scene graphs from get_sceneGraphs.py

--- code_thesis/Replica_code/README.md
+++ code_thesis/Replica_code/README.md
@@ -1,3 +1,5 @@
 First of all, follow the instructions in `segmentation_and_postprocessing`.
 
 Then, get the scene graphs with `get_sceneGraphs.py`. RENAME THE FOLDERS, AFTERWARDS, since they are only saved with the indications on date and time.
+
+Then go to `track_changes`.
--- code_thesis/Replica_code/get_sceneGraphs.py
+++ code_thesis/Replica_code/get_sceneGraphs.py
@@ -4,13 +4,15 @@
 
 from SceneGraph import SceneGraph # local file
 import os
+import time
 
+start_time = time.time()
 
 #
 # Variables
 #
 
-frl_apartment = 'frl_apartment_1'
+frl_apartment = 'frl_apartment_0'
 basePath = '/local/home/gmarsich/Desktop/data_Replica/'
 
 namePCD = frl_apartment + '_withIDs.ply'
@@ -46,4 +48,23 @@ sceneGraph.populate_SceneGraph(path_plyFile, path_distanceMatrix, path_associati
 sceneGraph.save_SceneGraph(path_save_sceneGraph)
 
 
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.6f} seconds")
+
+
+# TIMINGS:
+# frl_apartment_0 GT: 26.915117 seconds
+# frl_apartment_1 GT: 27.084913 seconds
+# frl_apartment_2 GT: 28.079581 seconds
+# frl_apartment_3 GT: 27.812196 seconds
+# frl_apartment_4 GT: 28.006293 seconds
+# frl_apartment_5 GT: 26.807828 seconds
+
+# frl_apartment_0 LabelMaker: 23.614874 seconds
+# frl_apartment_1 LabelMaker: 20.076981 seconds
+# frl_apartment_2 LabelMaker: 23.937329 seconds
+# frl_apartment_3 LabelMaker: 23.694971 seconds
+# frl_apartment_4 LabelMaker: 21.406928 seconds
+# frl_apartment_5 LabelMaker: 21.267375 seconds
 
